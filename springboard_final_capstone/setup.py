e5b66e706af55fe264f10535b8b92b07f9f94a16
trentleslie <trentleslie@gmail.com>
2021-11-22 17:20:33 -0800

    cookiecutter init

---
+++ springboard_final_capstone/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='LSTM Neural Network Candlestick Forecasting for Short-term Trading Opportunities',
+    author='Trent Leslie',
+    license='MIT',
+)
